5ca48a806f315e6446b67f14935491a9169c978e
unknown <ahamedraju152@gmail.com>
2025-05-25 13:09:20 +0600

    added a google map

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Map } from '@vis.gl/react-google-maps'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>React Google Map</h1>
+      <Map
+        style={{ width: '100vw', height: '100vh' }}
+        defaultCenter={{ lat: 23.8041, lng: 90.4152 }}
+        defaultZoom={10}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
+
+    </>
+  )
+}
+
+export default App
